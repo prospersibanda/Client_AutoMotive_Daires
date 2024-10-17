6cecb76b64a31e84785be61f916ee00c57ad1651
Prosper Sibanda <prosper.thecoder@gmail.com>
2024-10-17 13:33:31 +0200

    installed thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-=======
-import { createStore, combineReducers } from 'redux';
-import blogReducer from './blogReducer';
-import categoryReducer from './categoryReducer';
-import authReducer from './authReducer';
->>>>>>> adf7854e0ec4d706e66bb64ef4286f27d0a2e096
 
 // Import your reducers
 import blogReducer from './reducers/blogReducer';
@@ -18,12 +11,7 @@ import commentReducer from './reducers/commentReducer';
 const rootReducer = combineReducers({
   auth: authReducer,
   blogs: blogReducer,
-<<<<<<< HEAD
   comments: commentReducer,
-=======
-  categories: categoryReducer,
-  auth: authReducer,
->>>>>>> adf7854e0ec4d706e66bb64ef4286f27d0a2e096
 });
 
 // Create the Redux store
